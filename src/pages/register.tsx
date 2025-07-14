a78f11f104d6abaecaa5927ba104ab43b86efaf2
Petko Latev <Petkorlatev@gmail.com>
2025-07-14 14:44:31 +0300

    change name of function,page name and button name. change component name

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import styles from '../styles/Login.module.css'
 import { useRouter } from 'next/router';
 
-export default function createUser() {
+export default function signUp() {
     const router = useRouter()
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
@@ -15,13 +15,13 @@ export default function createUser() {
     }
     return (
         <div className={styles.login}>
-            <h2>Create User</h2>
+            <h2>Sign Up</h2>
             <form onSubmit={handleCreate}>
                 <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
                 <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Name" />
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
                 <input type="password" value={rePassword} onChange={(e) => setRePassword(e.target.value)} placeholder="RePassword" />
-                <button type="submit">Create</button>
+                <button type="submit">Submit</button>
                 {msg && <p>{msg}</p>}
             </form>
         </div>
