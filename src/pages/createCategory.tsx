57a3bfe853e45fd4816206b61cf228e027169267
Petko Latev <Petkorlatev@gmail.com>
2025-09-01 12:56:14 +0300

    add Popup for adding new merchant,changes on css

--- src/pages/createCategory.tsx
+++ src/pages/createCategory.tsx
@@ -1,175 +1,152 @@
 import { useDataContext } from '@/context/CategoryContext';
-import styles from '../styles/Category.module.css'
+import styles from '../styles/Category.module.css';
 import { useState, FormEvent } from 'react';
-
+import Modal from '@/components/Modal';
 
 export default function CreateCategoryPage() {
-    const [activeCategory, setActiveCategory] = useState<string | null>(null)
-    const [createCategory, setCreateCategory] = useState(false)
-    const [name, setName] = useState<string>('');
-    const [merchantName, setMerchantName] = useState<string>('');
-    const [description, setDescription] = useState<string>('');
-    const [message, setMessage] = useState<string>('');
-    const { categories, reloadCategories } = useDataContext()
+  const [createCategory, setCreateCategory] = useState(false);
+  const [showMerchantModal, setShowMerchantModal] = useState(false);
+  const [name, setName] = useState<string>('');
+  const [merchantName, setMerchantName] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+  const { categories, reloadCategories } = useDataContext();
 
-    const handleCategorySubmit = async (e: FormEvent) => {
-        e.preventDefault();
+  const handleCategorySubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const res = await fetch('/api/createCategories', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name }),
+    });
 
-        const res = await fetch('/api/createCategories', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                name
-            }),
-        });
-
-        const data = await res.json();
-        if (data.success) {
-            setMessage(data.message);
-            setName('');
-            reloadCategories()
-            setCreateCategory(false)
-        } else {
-            setMessage(`Error: ${data.error}`);
-        }
-    };
-
-    const handleMerchantSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-
-        const res = await fetch('/api/createMerchants', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                name,
-                merchantName,
-                description
-            }),
-        });
-
-        const data = await res.json();
-        if (data.success) {
-            setMessage(data.message);
-            setName('');
-            setMerchantName('');
-            setDescription('')
-            reloadCategories()
-            setCreateCategory(false)
-            setActiveCategory(null)
-        } else {
-            setMessage(`Error: ${data.error}`);
-        }
-    };
-
-    const handleDelete = async (categoryName: string, merchantName: string) => {
-        const deletedItem = merchantName ? merchantName : categoryName
-        const confirmed = window.confirm(`Are you sure you want to delete ${deletedItem} ?`)
-
-        if (!confirmed) return
-
-        const res = await fetch('/api/deleteMerchant', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                categoryName,
-                merchantName,
-            }),
-        });
-
-        if (res.status === 200) {
-            reloadCategories()
-        }
-
-
+    const data = await res.json();
+    if (data.success) {
+      setMessage(data.message);
+      setName('');
+      reloadCategories();
+      setCreateCategory(false);
+    } else {
+      setMessage(`Error: ${data.error}`);
     }
+  };
 
-    return (
-        <div>
-            <div className={styles.category}>
-                <button onClick={() => setCreateCategory(!createCategory)}>Добави категория</button>
-                {createCategory ?
-                    <div className={styles.category}>
-                        <form onSubmit={handleCategorySubmit}>
-                            <label>
-                                Име на категория:
-                                <input value={name} onChange={e => setName(e.target.value)} required />
-                            </label>
-                            <button type="submit">Създай</button>
-                        </form>
-                        {message && <p className={styles.msg}>{message}</p>}
-                    </div>
-                    : ''
-                }
-            </div>
-            <div className={styles.category}>
-                <h1>Категории и търговци</h1>
-                <div className={styles.deleteCategory}>
-                    {categories.map((group, categoryIndex) => {
-                        const [categoryName, ...merchants] = group;
+  const handleMerchantSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    const res = await fetch('/api/createMerchants', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, merchantName, description }),
+    });
 
-                        return (
-                            <div key={categoryName} className={styles.categoryName}>
+    const data = await res.json();
+    if (data.success) {
+      setMessage(data.message);
+      setName('')
+      setMerchantName('');
+      setDescription('');
+      setShowMerchantModal(false);
+      reloadCategories();
+    } else {
+      setMessage(`Error: ${data.error}`);
+    }
+  };
 
-                                <h2>{categoryName} <button onClick={() => handleDelete(categoryName, merchantName)}>
-                                    X
-                                </button></h2>
+  const handleDelete = async (categoryName: string, merchantName: string) => {
+    const deletedItem = merchantName || categoryName;
+    const confirmed = window.confirm(`Сигурен ли си, че искаш да изтриеш ${deletedItem}?`);
+    if (!confirmed) return;
 
-                                <button className={styles.addMerchant}
-                                    onClick={() => {
-                                        setActiveCategory(prev => (prev === categoryName ? null : categoryName))
-                                        if (!activeCategory) {
-                                            setName(categoryName)
-                                        } else {
-                                            setName('')
-                                        }
-                                    }
-                                    }
-                                >Добави търговец</button>
-                                {activeCategory === categoryName && (
-                                    <form onSubmit={handleMerchantSubmit}>
-                                        <label>
-                                            Име на Търговец:
-                                            <input
-                                                value={merchantName}
-                                                onChange={e => setMerchantName(e.target.value)}
-                                                required
-                                            />
-                                        </label>
-                                        <br />
-                                        <label>
-                                            Описание:
-                                            <textarea
-                                                value={description}
-                                                onChange={e => setDescription(e.target.value)}
-                                                rows={4}
-                                            />
-                                        </label>
-                                        <br />
-                                        <button type="submit">Създай</button>
-                                        {message && <p className={styles.msg}>{message}</p>}
-                                    </form>
-                                )}
+    const res = await fetch('/api/deleteMerchant', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ categoryName, merchantName }),
+    });
 
-                                <ul>
-                                    {merchants.map((merchant, merchantIndex) => (
-                                        <li key={merchant}>
-                                            <span>{merchant}</span>
+    if (res.status === 200) {
+      reloadCategories();
+    }
+  };
 
-                                            <button
-                                                onClick={() => handleDelete(categoryName, merchant)}
-                                            >
-                                                X
-                                            </button>
+  return (
+    <div>
+      <div className={styles.category}>
+        <button onClick={() => {setCreateCategory(!createCategory),setName('')}}>Добави категория</button>
+        {createCategory && (
+          <div className={styles.category}>
+            <form onSubmit={handleCategorySubmit}>
+              <label>
+                Име на категория:
+                <input value={name} onChange={e => setName(e.target.value)} required />
+              </label>
+              <button type="submit">Създай</button>
+            </form>
+            {message && <p className={styles.msg}>{message}</p>}
+          </div>
+        )}
+      </div>
 
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        );
-                    })}
-                </div>
+      <div className={styles.category}>
+        <h1>Категории и търговци</h1>
+        <div className={styles.deleteCategory}>
+          {categories.map(group => {
+            const [categoryName, ...merchants] = group;
 
-            </div>
+            return (
+              <div key={categoryName} className={styles.categoryName}>
+                <h2>
+                  {categoryName}{' '}
+                  <button onClick={() => handleDelete(categoryName, '')}>X</button>
+                </h2>
+
+                <button
+                  className={styles.addMerchant}
+                  onClick={() => {
+                    setName(categoryName);
+                    setShowMerchantModal(true);
+                  }}
+                >
+                  Добави търговец
+                </button>
+
+                <ul>
+                  {merchants.map(merchant => (
+                    <li key={merchant}>
+                      <span>{merchant}</span>
+                      <button onClick={() => handleDelete(categoryName, merchant)}>X</button>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
         </div>
-    );
+      </div>
+
+      <Modal isOpen={showMerchantModal} onClose={() => setShowMerchantModal(false)}>
+        <form className={styles.PopUp} onSubmit={handleMerchantSubmit}>
+          <label>
+            Име на Търговец:
+            <input
+              value={merchantName}
+              onChange={e => setMerchantName(e.target.value)}
+              required
+            />
+          </label>
+          <br />
+          <label>
+            Описание:
+            <textarea
+              value={description}
+              onChange={e => setDescription(e.target.value)}
+              rows={4}
+            />
+          </label>
+          <br />
+          <button type="submit">Създай</button>
+          {message && <p className={styles.msg}>{message}</p>}
+        </form>
+      </Modal>
+    </div>
+  );
 }
