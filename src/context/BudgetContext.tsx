c4d2491ba2a5a55b99b4498bcf6a576deb70c3a1
Petko Latev <Petkorlatev@gmail.com>
2025-09-17 11:29:57 +0300

    remove unused thinks

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext, useState, useEffect } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
 import { Transaction } from '@/types/types';
 
 type BudgetProviderProps = {
