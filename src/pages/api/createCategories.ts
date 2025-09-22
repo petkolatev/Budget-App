1dc574838e03ca655c613bbaa18e41cf4e5319f3
Petko Latev <Petkorlatev@gmail.com>
2025-09-22 13:39:36 +0300

    create error type function checker

--- src/pages/api/createCategories.ts
+++ src/pages/api/createCategories.ts
@@ -1,37 +1,32 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import connectDB from '@/lib/mongodb';
-import { Category } from '@/types/Category';
+import type { NextApiRequest, NextApiResponse } from "next";
+import connectDB from "@/lib/mongodb";
+import { Category } from "@/types/Category";
+import typeErrorMessage from "@/utils/typeErrorMessage";
 
 type Data =
-    | { success: true; message: string }
-    | { success: false; error: string };
+  | { success: true; message: string }
+  | { success: false; error: string };
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
 ) {
-    await connectDB();
+  await connectDB();
 
-    if (req.method === 'POST') {
-        const { name } = req.body;
+  if (req.method === "POST") {
+    const { name } = req.body;
 
-        if (!name) {
-            return res.status(400).json({ success: false, error: 'Invalid data' });
-        }
-
-        try {
-            await Category.create({ name })
-            res.status(200).json({ success: true, message: 'Created new category' });
-
-        } catch (error: unknown) {
-            let errorMessage = 'Server error'
-
-            if (error instanceof Error) {
-                errorMessage = error.message
-            }
-            res.status(500).json({ success: false, error: errorMessage });
-        }
-    } else {
-        res.status(405).json({ success: false, error: 'Method not allowed' });
+    if (!name) {
+      return res.status(400).json({ success: false, error: "Invalid data" });
     }
+
+    try {
+      await Category.create({ name });
+      res.status(200).json({ success: true, message: "Created new category" });
+    } catch (error: unknown) {
+      res.status(500).json({ success: false, error: typeErrorMessage(error) });
+    }
+  } else {
+    res.status(405).json({ success: false, error: "Method not allowed" });
+  }
 }
